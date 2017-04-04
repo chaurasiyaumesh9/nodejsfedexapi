09f54419883ea000e3739ce4c37e257b66108517
chaurasiyaumesh9 <umesh.chaurasiya@adapty.com>
2017-04-04 22:39:18 +0530

    header added

--- server.js
+++ server.js
@@ -12,6 +12,25 @@ app.use(bodyParser.json()) // parse application/json
 app.use(session({ secret: 'meanstack passport app' })); // session secret
 app.use(bodyParser.urlencoded({ extended: true })) // parse application/x-www-form-urlencoded
 
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://checkout.sandbox.netsuite.com/');
+
+    /*// Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+*/
+    // Pass to next layer of middleware
+    next();
+});
 app.get('/', function(req, res) {
   res.send("hellow world!");
 });
