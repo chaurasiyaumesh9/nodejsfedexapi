4335d0eccff55e1fd22763dd46b9baabffa445c1
chaurasiyaumesh9 <umesh.chaurasiya@adapty.com>
2017-04-05 13:13:13 +0530

    paramters added to accept user format

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var port = process.env.PORT || 3000;
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()) // parse application/json
 app.use(session({ secret: 'meanstack passport app' })); // session secret
-app.use(bodyParser.urlencoded({ extended: true })) // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false })) // parse application/x-www-form-urlencoded
 app.use(cors())
 
 
@@ -35,15 +35,88 @@ app.use(function (req, res, next) {
     next();
 });
 app.get('/', function(req, res) {
-  res.send("hellow world!");
+  res.send("hello world!");
 });
 
 
-app.get('/rates/:zip', function(request, response) {
+app.post('/rates', function(request, response) {
   //res.send("hellow world!");
-  var zip = request.params.zip;
+  //var zip = request.params.zip;
   //res.send(zip);
-  if (zip) {
+ // console.log('body : ',request.body.fedexData);
+  if (request.body.fedexData) {
+  	var input = request.body.fedexData;
+  	var Shipper = input['Shipper'] || {
+			Contact: {
+				PersonName: 'Sender Name',
+				CompanyName: 'Primary Arms',
+				PhoneNumber: '5555555555'
+			},
+			Address: {
+				StreetLines: ['2825 Miller Ranch Rd.'],
+				City: 'Pearland',
+				StateOrProvinceCode: 'TX',
+				PostalCode: '77584',
+				CountryCode: 'US'
+			}
+		}, 
+  		Recipient = input['Recipient'] || {
+			Contact: {
+				PersonName: 'Recipient Name',
+				CompanyName: 'Company Receipt Name',
+				PhoneNumber: '5555555555'
+			},
+			Address: {
+				StreetLines: ['Address Line 1'],
+				PostalCode: '77407',
+				CountryCode: 'US'
+			}
+		}, 
+  		PackageCount = input['PackageCount'] || '1', 
+  		RequestedPackageLineItems = input['RequestedPackageLineItems'] || {
+	      SequenceNumber: 1,
+	      GroupPackageCount: 1,
+	      Weight: {
+	        Units: 'LB',
+	        Value: '50.0'
+	      },
+	      Dimensions: {
+	        Length: 108,
+	        Width: 5,
+	        Height: 5,
+	        Units: 'IN'
+	      }
+	    };
+
+  	var params = {};
+  	params['ReturnTransitAndCommit'] = true;
+  	params['CarrierCodes'] = ['FDXE','FDXG'];
+  	params['RequestedShipment'] = {};
+  	params['RequestedShipment']['DropoffType'] = "REGULAR_PICKUP";
+  	params['RequestedShipment']['PackagingType'] = "YOUR_PACKAGING";
+  	params['RequestedShipment']['Shipper'] = Shipper;
+  	params['RequestedShipment']['Recipient'] = Recipient;
+  	params['RequestedShipment']['ShippingChargesPayment'] = {
+      PaymentType: 'SENDER',
+      Payor: {
+        ResponsibleParty: {
+          AccountNumber: fedex.options.account_number
+        }
+      }
+    };
+    params['RequestedShipment']['PackageCount'] = PackageCount;
+    params['RequestedShipment']['RequestedPackageLineItems'] = RequestedPackageLineItems;
+
+    fedex.rates(params, function(err, results) {
+	  if(err) {
+	    //return console.log(err);
+	    response.json(err);
+	  }
+	  //console.log(results);
+	  response.json(results);
+	});
+
+    /*
   	fedex.rates({
 	  ReturnTransitAndCommit: true,
 	  CarrierCodes: ['FDXE','FDXG'],
@@ -116,8 +189,10 @@ app.get('/rates/:zip', function(request, response) {
 	  //console.log(results);
 	  response.json(results);
 	  
-	});
+	});*/
   };
+  //response.send(request.body);
+
 });
 
 app.listen(port ,function(){
